5600cb282e1e344229f13404e4ed9221d5a51b64
RuslanIvanov <rus_iv@list.ru>
2015-09-21 07:50:28 -0700

    21_09_15

---
+++ 002/main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc,char* argv[], char** env)
+{
+	char buf[100];
+
+	while(*env++) //*env != NULL
+	{
+		printf("\n%s",*env);
+		char * p = strstr(*env,"PWD=");
+		if(*env==p) 
+		{
+			printf("\n current path: %s",p+4);
+			break;
+		}
+	}
+
+    	return 0;
+}

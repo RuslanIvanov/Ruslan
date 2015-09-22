d8ed4f78551df4174ff1210986664bfc5b000a0d
RuslanIvanov <rus_iv@list.ru>
2015-09-22 06:25:07 -0700

    22_09_2015

--- 001/hello.c
+++ 001/hello.c
@@ -1,28 +1,7 @@
-//#define SHARED_LIB
 #include "print.h"
-#include <dlfcn.h>
 
 int main()
 {
     printf("\n\tHello world\n");
-#ifdef SHARED_LIB
     myprint();
-#else
-	void *library_handler;
-	void (*pmyprint)();
-
-	library_handler = dlopen("./libprint.so",RTLD_LAZY);
-	if (!library_handler)
-	{
-		printf("dlopen() error: %s\n", dlerror());
-		exit(1);
-	}
-
-	pmyprint = dlsym(library_handler,"myprint");
-
-	(*pmyprint)();
-
-	dlclose(library_handler);
-
-#endif
 }

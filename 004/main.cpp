24fe7d5ea38a2ee5b6d8c3b056ea0392ece7e914
RuslanIvanov <rus_iv@list.ru>
2015-10-02 09:31:49 +0300

    2_10_15

--- 004/main.cpp
+++ 004/main.cpp
@@ -7,8 +7,16 @@
 using namespace std;
 
 int getppid(int);
+void getname(int,char*);
 void listTree(int pidStart);
-void printList(list<int>&List);
+
+struct process
+{
+	char name[50];
+	int pid;
+};
+
+void printList(list<process>&List);
 
 int main(int argc,char* argv[], char** env)
 {
@@ -16,12 +24,16 @@ int main(int argc,char* argv[], char** env)
 	{	int pid = atoi(argv[1]);
 
 		printf("\n*****************************************\n");
-		printf("\nThe parent %d is %d\n",pid,getppid(pid));
-		printf("\n*****************************************\n");
+		char name[50];
+		getname(pid,name);
+		printf("\nThe parents %d-{%s} is:\n",pid,name);
 
 		listTree(pid);
+
+		printf("\n*****************************************\n");
+
 	}
-	else 
+	else
 	{
 	    listTree(0);
 	}
@@ -32,11 +44,9 @@ int getppid(int pid)
 {
 	if(pid==0) {printf("Error: getppid: number pid!"); return 0;}
 
-	char dir[10]="/proc/";
-	char file[10]="/status";
 	char buf[256];
 
-	sprintf(buf,"%s%d%s",dir,pid,file);
+	sprintf(buf,"/proc/%d/status",pid);
 	//printf("\nIm is finding parent in %s\n",buf);
 
 	FILE* fd=0;
@@ -57,18 +67,15 @@ int getppid(int pid)
 			    bufString[i] = '\0';
 			    i=0;
 			    char * pfind = strstr(bufString,"PPid:");
-//			    printf("\npfind=%p [%s] [%s]",pfind,pfind,bufString);
 
             		    if(pfind!=NULL)
-            		    {	char buf[256];
-                    		//yprintf("\nI'm find string: %s\n",pfind);
+            		    {
+				char buf[256];
 				sscanf(bufString,"%s%d",&buf[0],&ppid);
-				//printf("\npppid = %d",ppid);
 				break;
 			    }
-
      		 }
-		i++;
+		 i++;
 	}
 
 	if(fd!=0)
@@ -77,23 +84,67 @@ int getppid(int pid)
 	return ppid;
 }
 
+void getname(int pid,char* pn)
+{
+
+	char buf[256];
+        sprintf(buf,"/proc/%d/status",pid);
+
+ 	FILE* fd=0;
+        fd = fopen(buf,"r");
+        if(fd==NULL){printf("\nError fopen [%s]",buf); return;}
+
+        int ch = 0;
+        int i=0;
+        char bufString[256];
+
+        while((ch=fgetc(fd)) != EOF)
+        {
+		bufString[i] = ch;
+                if((char)ch == 0x0a)
+                {
+
+                           bufString[i] = '\0';
+                           i=0;
+
+                           char * pname = strstr(bufString,"Name:");
+                           if(pname!=NULL)
+                           {
+                                char buf[256]; char name[100];
+                                sscanf(bufString,"%s%s",&buf[0],&name[0]);
+                                strcpy(pn,name);
+				break;
+                           }
+		}
+		i++;
+	}
+
+	if(fd!=0)
+        fclose(fd);
+}
+
 void listTree(int pidStart)
 {
-	list<int> dirList;
+	list<process> dirList;
 	int ppid = -1;
 
 	while(getppid(pidStart)!=0)
 	{
 		ppid = getppid(pidStart);
 		pidStart = ppid;
-		dirList.push_back(ppid);
+
+		process pr;
+		char name[50];
+		getname(ppid,name);
+		pr.pid=ppid;
+		strcpy(pr.name,name);
+		dirList.push_back(pr);
 	}
 
 	printList(dirList);
 }
 
-
-void printList(list<int>&List)
+void printList(list<process>&List)
 {
 	if(List.empty())
 	{
@@ -101,11 +152,13 @@ void printList(list<int>&List)
 		return;
 	}
 
-	list<int>::iterator iter;
+	list<process>::iterator iter;
 
 	for(iter=List.begin();iter!=List.end();iter++)
 	{
-		printf("\t-- %d\n",*iter);
+		printf("\n%10d-{%s}",(*iter).pid,(*iter).name);
 	}
+
+	printf("\n");
 }
 

8b8d780562ea6681694dd68b03197b60606cfc82
RuslanIvanov <rus_iv@list.ru>
2015-10-06 09:26:17 +0300

    6_10_15:9.26

--- 007/main.cpp
+++ 007/main.cpp
@@ -3,17 +3,55 @@
 #include <string.h>
 #include <pthread.h>
 
-void * funcThread(void*)
-struct Peram
+void * funcThread(void*);
+struct Param
 {
     int num;
     double left;
     double right;
 };
+int S;
 
 int main(int argc,char* argv[], char** env)
 {
-	pthred_t thId = 0;
-	pthread_create (&thId, NULL, funcThread, p);
+	if(argc>1)
+	{
+		int N = atoi(argv[1]);
+
+		Param *pparam = new Param[N];
+		pthread_t *pthId = new pthread_t[N];
+
+		for(int i=1;i<=N;i++)
+		{
+			pparam[i].num = i;
+			pthread_create (pthId+i, NULL, funcThread, pparam+i);
+		}
+
+		void* vs=0;int s=0;
+		printf("\nWait...");
+		for(int i=1;i<=N;i++)
+		{
+			pthread_join(pthId[i],&vs);
+			s=(int)vs;
+			S+=s;
+		}
+
+		delete [] pthId; 
+		delete [] pparam;
+	}
+
+	printf("S= %d, Exit...",S);
 	return 0;
+}
+
+void * funcThread(void* inputPar)
+{
+	int s=0;
+	Param *p = (Param*)inputPar;
+	printf("\n\trun thread %d ",p->num);
+
+	s=1;
+
+	return (void*)s;
+
 }
---
+++ 007/readme.txt
@@ -0,0 +1,7 @@
+#error:
+
+Wait...
+    run thread 2 
+    run thread 3 
+*** Error in `./main': munmap_chunk(): invalid pointer: 0x08dcb048 ***
+    run thread 1 Аварийный останов (сделан дамп памяти)

c0a34d2f4187028f8d01fd74786a555609237e35
RuslanIvanov <rus_iv@list.ru>
2015-10-06 08:36:35 -0700

    6_10_15

---
+++ 009/main.cpp
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void invert(char*);
+
+int main(int argc,char* argv[], char** env)
+{
+
+	if(argc>1)
+	{
+	FILE *fp;
+	char temp[256];
+	strcpy(temp,argv[1]);
+	printf("\nargc %d\n",argc);
+	for(int i=2;i<argc;i++)
+	{
+		strcat(&temp[0]," ");
+		strcat(&temp[0],argv[i]);
+	}
+
+	printf("\ncmd: [%s]",temp);
+
+	fp=popen(temp, "r");
+	char buf[2256];
+
+	char c;int i=0;
+	while ((c = fgetc(fp)) != EOF) 
+	{
+	    buf[i] = c;
+	    if(buf[i]=='\n')
+	    {
+		buf[i]='\0';
+		printf("\n%s",buf);
+		invert(buf);
+		i=0;
+	    }
+	    i++;
+	}
+	
+
+	pclose(fp);
+	}
+	printf("\n\nExit...\n");
+	return 0;
+}
+
+void invert(char* buf)
+{
+	int len = strlen(buf);
+	for(int ii=0;ii<len/2;ii++)
+	{
+	    char temp = buf[ii];
+	    buf[ii] = buf[len-ii-1];
+	    buf[len-ii-1] = temp;
+	}
+	printf(" %s",buf);
+}

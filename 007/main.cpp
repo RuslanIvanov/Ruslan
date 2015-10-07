25835288a1e370bcb765dbbd1b6e1cba6e27f094
RuslanIvanov <rus_iv@list.ru>
2015-10-07 22:23:51 +0300

    7_10_15:22.23

--- 007/main.cpp
+++ 007/main.cpp
@@ -17,19 +17,23 @@ int main(int argc,char* argv[], char** env)
 	if(argc>1)
 	{
 		int N = atoi(argv[1]);
+		int n = atoi(argv[2]);
 
-		Param *pparam = new Param[N];
-		pthread_t *pthId = new pthread_t[N];
+		Param *pparam = new Param[n];
+		pthread_t *pthId = new pthread_t[n];
 
-		for(int i=0;i<N;i++)
+		for(int i=0;i<n;i++)
 		{
 			pparam[i].num = i;
+			pparam[i].left = ((N/n) - 1)*i;
+			pparam[i].right = ((N/n) + 1)*i;
+
 			pthread_create (pthId+i, NULL, funcThread, pparam+i);
 		}
 
 		void* vs=0;int s=0;
 		printf("\nWait...");
-		for(int i=0;i<N;i++)
+		for(int i=0;i<n;i++)
 		{
 			pthread_join(pthId[i],&vs);
 			s=(int)vs;
@@ -50,7 +54,7 @@ void * funcThread(void* inputPar)
 	Param *p = (Param*)inputPar;
 	printf("\n\trun thread %d\n",p->num);
 
-	s=1;
+	s=p->left + p->right;
 
 	return (void*)s;
 

7b375c289ea97d7f64a597edf0ee6550c94120af
RuslanIvanov <rus_iv@list.ru>
2015-10-09 09:14:32 +0300

    9_10_15

--- 007/main.cpp
+++ 007/main.cpp
@@ -2,60 +2,85 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
+#include <math.h>
 
 void * funcThread(void*);
 struct Param
 {
-    int num;
-    double left;
-    double right;
+    double a;
+    double h;
+    int i;
 };
-int S;
+
+double S;
+double *pS;
+
+double f(double x)
+{
+      return sin(x);
+}
 
 int main(int argc,char* argv[], char** env)
 {
-	if(argc>1)
+	if(argc==3)
 	{
-		int N = atoi(argv[1]);
-		int n = atoi(argv[2]);
+		double a = 0; //Левая граница
+		double b = atoi(argv[2]);// правая граница
+		int n = atoi(argv[1]);// кол_во разбиений
 
-		Param *pparam = new Param[n];
-		pthread_t *pthId = new pthread_t[n];
+		if(n<=0){printf("\nError: number of partitions\n\n"); return 0;} 
 
-		for(int i=0;i<n;i++)
-		{
-			pparam[i].num = i;
-			pparam[i].left = ((N/n) - 1)*i;
-			pparam[i].right = ((N/n) + 1)*i;
+		double h=0.0;
+		Param *pparam = NULL;
+		pthread_t *pthId = NULL;
 
-			pthread_create (pthId+i, NULL, funcThread, pparam+i);
-		}
+    		h=(b-a)/(double)n; // шаг интегрирования
 
-		void* vs=0;int s=0;
+		pparam = new Param[n];
+		pthId = new pthread_t[n];
+		pS = new double [n];
+
+		for(int ii=0;ii<n-1;ii++)
+    		{
+			pparam[ii].a = a;
+			pparam[ii].h = h;
+			pparam[ii].i= ii;
+
+			pthread_create(pthId+ii, NULL, funcThread, pparam+ii);
+    		}
+
+		printf("\nstep %6.3f",h);
 		printf("\nWait...");
-		for(int i=0;i<n;i++)
+		for(int i=0;i<n-1;i++)
 		{
-			pthread_join(pthId[i],&vs);
-			s=(int)vs;
-			S+=s;
-		}
+			pthread_join(pthId[i],NULL);
 
+		}
+		printf("\ncalculated!\n");
+		for(int i=0;i<n-1;i++)
+                {
+                	S+=pS[i];
+                }
+
+		S=S*h;
 		delete [] pthId; 
 		delete [] pparam;
-	}
+		delete [] pS;
 
-	printf("\nS = %d, Exit...\n",S);
+	}else {printf("\nError: set command string: [number of partitions] [right limit]\n\n"); return 0;} 
+
+	printf("\nS = %6.3f\nExit!\n\n",S);
 	return 0;
 }
 
 void * funcThread(void* inputPar)
 {
-	int s=0;
+	double x=0.;
 	Param *p = (Param*)inputPar;
-	printf("\n\trun thread %d\n",p->num);
 
-	s=p->left + p->right;
-
-	return (void*)s;
-
+	x=(p->a)+(p->i)*(p->h);
+        pS[p->i]=f(x);
+	//printf("\nS[%d] = %6.3f",p->i,pS[p->i]);
+	return 0;
 }
+

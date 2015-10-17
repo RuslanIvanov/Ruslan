0e42e8f7d2219608b8d4e5a9b841178434ad2927
RuslanIvanov <rus_iv@list.ru>
2015-10-18 00:22:55 +0300

    555

--- 015/client.cpp
+++ 015/client.cpp
@@ -9,38 +9,80 @@
 #include <fcntl.h>
 #include <pthread.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 #include <netinet/in.h>
 
+char nameClient[BUFSIZ];
+unsigned int port;
+void out(int sig=0);
+bool bOut = false;
 
-char message[] = "Hello there!\n";
-char buf[sizeof(message)];
-
-int main()
+int main(int argc, char* argv[])
 {
-    int sock;
-    struct sockaddr_in addr;
+    signal (SIGTERM, out);
+    signal (SIGINT, out);
 
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    if(sock < 0)
+    struct sockaddr_in addr;
+    struct in_addr adrDst;
+    memset(&addr,0,sizeof(struct sockaddr_in));
+    memset(&adrDst,0,sizeof(struct in_addr));
+
+    if(argc==3)
+    {
+	if(inet_pton(AF_INET, argv[1],(void*)&adrDst)<=0)
+	{perror("Error ip: "); return 0;}
+
+	port = atoi(argv[2]);
+	if(port<=0){printf("\nError number port"); return 0;}
+
+	addr.sin_family = AF_INET;
+    	addr.sin_port = htons(port); 
+	addr.sin_addr = adrDst;
+	strcpy(nameClient,argv[3]); 
+    }else { printf("\nSet command string: 'ip-addres' 'port' 'name'"); return 0; }
+
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if(sockfd < 0)
     {
         perror("socket");
-        exit(1);
+        return 0;
     }
 
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(3425); // или любой другой порт...
-    addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-    if(connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    if(connect(sockfd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
     {
         perror("connect");
-        exit(2);
+        return 0;
     }
 
-    send(sock, message, sizeof(message), 0);
-    recv(sock, buf, sizeof(message), 0);
-    
-    printf(buf);
-    close(sock);
+    while(bOut==false)
+    {
+		char buf[BUFSIZ] = {'\0'};
+
+		printf("\nsend msg client  ' %s ': ",nameClient);
+		fgets(buf,BUFSIZ,stdin);
+		sprintf(buf,"%s:%s",nameClient,buf);
+		int i = strlen(buf)-1;
+
+		if(buf[i] == '\n') buf[i]= '\0';
+
+    		send(sockfd, buf, sizeof(buf), 0);
+    		recv(sockfd, buf, sizeof(buf), 0);
+		
+		printf("\n\tread msg: %s",buf);		
+    }
+
+    shutdown(sockfd, 2);
+    close(sockfd);
 
     return 0;
+}
+
+void out(int sig)
+{
+    if(sig==SIGTERM||sig == SIGINT)
+    {
+	printf("\nGoodbye ' %s '\n",nameClient);
+	bOut=true;
+    }
 }
--- 015/server.cpp
+++ 015/server.cpp
@@ -9,13 +9,52 @@
 #include <fcntl.h>
 #include <pthread.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 #include <netinet/in.h>
+#include <vector>
+#include <string>
 
-int main()
+#define MAX_CONNECT 100
+unsigned int port;
+void out(int sig=0);
+bool bOut = false;
+char nameServ[BUFSIZ];
+void * funcThread(void*);
+
+struct Param
 {
-    int sock, listener;
+      struct sockaddr_in addr;
+      int sockTh;
+      int n;
+};
+
+int main(int argc, char* argv[])
+{
+
+    signal (SIGTERM, out);
+    signal (SIGINT, out);
+
     struct sockaddr_in addr;
-    char buf[1024];
+    struct sockaddr_in addrCli[MAX_CONNECT];
+    memset(&addr,0,sizeof(struct sockaddr_in));
+    memset(&addrCli,0,sizeof(struct sockaddr_in)*MAX_CONNECT);
+
+    if(argc==2)
+    {
+	port = atoi(argv[1]);
+	if(port<=0){printf("\nError number port"); return 0;}
+
+	addr.sin_family = AF_INET;
+    	addr.sin_port = htons(port); 
+	
+	strcpy(nameServ,argv[2]); 
+    }else { printf("\nSet command string: 'port' 'name'"); return 0; }
+
+    int sock, listener;
+
+    pthread_t thId[MAX_CONNECT];
+    Param param[MAX_CONNECT];
+    char buf[BUFSIZ];
     int bytes_read;
 
     listener = socket(AF_INET, SOCK_STREAM, 0);
@@ -24,36 +63,70 @@ int main()
         perror("socket");
         exit(1);
     }
-    
+
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(3425);
+    addr.sin_port = htons(port);
     addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
     if(bind(listener, (struct sockaddr *)&addr, sizeof(addr)) < 0)
     {
         perror("bind");
         exit(2);
     }
 
-    listen(listener, 1);
-    
-    while(1)
+    listen(listener, MAX_CONNECT);
+    int iConnect=0;
+    while(bOut==false)
     {
-        sock = accept(listener, NULL, NULL);
+	if(iConnect>=MAX_CONNECT) continue;
+
+	int nAddrCli=0;
+        sock = accept(listener, (struct sockaddr*)&addrCli[iConnect], (socklen_t *)&nAddrCli);
         if(sock < 0)
         {
             perror("accept");
-            exit(3);
+            return 0;
         }
+	
+	thId[iConnect] = pthread_create(&thId[iConnect], NULL, funcThread, &param[iConnect]);
 
-        while(1)
-        {
-            bytes_read = recv(sock, buf, 1024, 0);
-            if(bytes_read <= 0) break;
-            send(sock, buf, bytes_read, 0);
-        }
-    
-        close(sock);
+	iConnect++;
+      
     }
     
+	printf("\nWait threads...");
+	for(int i=0;i<iConnect;i++)
+	{
+		pthread_join(thId[i],NULL);
+		pthread_cancel(thId[i]);
+	}
+
     return 0;
+}
+
+void * funcThread(void* pVoid)
+{
+	int bytes_read=0;
+	Param *pparam = (Param*)pVoid;
+
+	while(bOut==false)
+        {
+	    char buf[BUFSIZ]={'\0'};
+            bytes_read = recv(pparam->sockTh, buf, BUFSIZ, 0);
+            if(bytes_read <= 0) continue;
+
+            send(pparam->sockTh, buf, bytes_read, 0);
+        }
+    
+        close(pparam->sockTh);
+	return 0;
+}
+
+void out(int sig)
+{
+    if(sig==SIGTERM||sig == SIGINT)
+    {
+	printf("\nGoodbye server'\n");
+	bOut=true;
+    }
 }

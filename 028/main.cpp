fb81aa987fbc823cedfc93d3eb8a5bb1e68f62a0
Ruslan <rus_iv@list.ru>
2015-11-18 23:42:57 +0300

    error

--- 028/main.cpp
+++ 028/main.cpp
@@ -27,30 +27,32 @@ int main(int argc,char* argv[], char** env)
 {
 
 	label1:
-
+	
 	if(pid==0 && i<N)// это дочерний процесс
 	{
 		sprintf(bufCatName,"./catMountProc_%d",i);
                 mkdir(bufCatName,555);
-		i++;
-
+		//i++;
 		pid = fork();// exception, sig = SIGCHLD ???
 
 		if(pid==0)
 		{///рекурсия должна быть
 			printf("\nI was a child process:\n");
 
-			if(unshare(CLONE_NEWPID)==-1)
-			{ perror("unshare"); return 0; }
+			//if(unshare(CLONE_SYSVSEM|CLONE_NEWPID)==-1)
+			//{ perror("unshare"); return 0; }
 
-			sleep(1);
-			if(mount("proc",bufCatName,"proc",0,NULL)==-1)
-			{ perror("mount"); return 0; }
+			//sleep(1);
+			//if(mount("proc",bufCatName,"proc",0,NULL)==-1)
+			//{ perror("mount"); return 0; }
 
 			printf("Mounting procfs at %s\n",bufCatName);
 			sleep(1);
 			printf("\npid child %d\n", getpid());
+			i++;
+			
 			goto label1;
+
 		}
 
 		if(pid==-1)
@@ -61,6 +63,7 @@ int main(int argc,char* argv[], char** env)
 			printf("\nI'm the parent process ' %d ',\nwait...", pid);
 			if (waitpid(pid, NULL, 0) == -1) 
 			perror("waitpid");
+			i=N;
 		}
 	}else {printf("Final child sleeping\n"); execlp("sleep", "sleep", "1000", (char *) NULL);}
 }

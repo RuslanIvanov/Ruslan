9212aab357a9a0238d5a83916900505f443d89ce
RuslanIvanov <rus_iv@list.ru>
2015-11-20 15:07:41 +0300

    8787

--- 025/main.cpp
+++ 025/main.cpp
@@ -27,36 +27,68 @@ int port = 12345;
 bool myping(char* buf,int bytes_read);
 bool myproto(char*,int *);
 
+struct icmp_header
+{
+	unsigned char type;
+	unsigned char code;
+	unsigned short crc;
+	unsigned short id;
+	unsigned short number;
+	unsigned char data[64];
+};
+
+icmp_header header_in;
+icmp_header header_out;
+
 int main(int argc, char* argv[])
 {
-
     signal (SIGTERM, out);
     signal (SIGINT, out);
 
     struct sockaddr_in addr;
+    struct in_addr adrDst;
+
 
     memset(&addr,0,sizeof(struct sockaddr_in));
+    memset(&adrDst,0,sizeof(struct in_addr));
 
     char bufOut[BUFSIZ];
     char bufIn[BUFSIZ];
 
-    int raw_sock = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
+    int raw_sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);//IPPROTO_RAW);
     if(raw_sock < 0)
     {perror("socket"); return 0;}
 
+    if(inet_pton(AF_INET, argv[1],(void*)&adrDst)<=0)
+    {perror("Error ip: "); return 0;}
+
+
     addr.sin_family = AF_INET;
     addr.sin_port = htons(port);
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_addr.s_addr = /*htonl*/adrDst.s_addr; // адрес хоста arg[1]
 
-    //int optval = 1;
-    //if(setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof optval)==-1){perror("SO_REUSEADDR:"); }
- 
+    int optval = 1;
+    if(setsockopt(listener, SOL_SOCKET, IP_HDRINCL, &optval, sizeof optval)==-1){perror("SO_REUSEADDR:"); }
+
+   header_out.type=0;
+   header_out.code = 8;
+   header_out.crc = 0;
+   header_out.id = getpid();
+   header_out.number = 0;
+
+   memcpy(header_in,0,sizeof(struct icmp_header));
+   header_in.type=0;
+   header_in.code = 0;
+
     int iConnect=0;
     while(bOut==false)
     {
 	int nSend;
 
 	ssize_t nsend = sendto(raw_sock, bufOut, nSend, MSG_DONTWAIT,(struct sockaddr *)&addr, sizeof(struct sockaddr_in));
+
+	header_out.number = iConnect;
+
 
 	int bytes_read  = recvfrom(raw_sock, bufIn, BUFSIZ, 0, NULL, NULL);
 
